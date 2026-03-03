09309a200878ab082cd1e56e18e79944fb9d5c01
ex1tttt <tkachmaksim2007@gmail.com>
2026-03-03 15:20:37 +0100

    feat: add 'mention' to notification type restriction

---
+++ db/migration_notifications_mention_type.sql
@@ -0,0 +1,8 @@
+-- Migration: add 'mention' to notifications type check constraint
+
+ALTER TABLE notifications
+  DROP CONSTRAINT IF EXISTS notifications_type_check;
+
+ALTER TABLE notifications
+  ADD CONSTRAINT notifications_type_check
+    CHECK (type IN ('follow', 'like', 'comment', 'comment_like', 'new_card', 'mention'));
